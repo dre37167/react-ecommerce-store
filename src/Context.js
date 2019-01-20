4cbfd80e42d7e287077989d6f40fddd4c8b0107e
dre37167 <tidre37167@gmail.com>
2019-01-20 14:13:09 -0700

    added bootstrap for style

--- src/Context.js
+++ src/Context.js
@@ -6,16 +6,53 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component{
   state={
-    products:storeProducts,
-    detailProduct:detailProduct
+    products:[],
+    detailProduct:detailProduct,
+    cart:[]
+  };
+
+  componentDidMount () {
+    this.setProducts();
   }
 
-  handleDetail = () =>{
+  setProducts = () =>{
+    let tempProducts = [];
+    storeProducts.forEach(item => {
+      const singleItem = {...item};
+      tempProducts = [...tempProducts, singleItem];
+    });
+    this.setState(() => {
+      return { products: tempProducts};
+    });
+  };
 
-  }
-  addToCart = () =>{
 
-  }
+  getItem = id =>{
+    const product = this.state.products.find(item => item.id === id);
+    return product;
+  };
+
+  handleDetail = (id) =>{
+    const product = this.getItem(id);
+    this.setState(()=>{
+      return {detailProduct:product}
+    })
+  };
+  addToCart = (id) =>{
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+
+    this.setState(()=>{
+      return {products:tempProducts, cart:[...this.state.cart,
+        product]}
+      },()=>{console.log("this.state")});
+
+  };
 
   render() {
     return (
--- src/components/Product.js
+++ src/components/Product.js
@@ -9,27 +9,35 @@ export default class Product extends Component{
     return(
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lgh-3 my-3">
       <div className="card">
-      <div className="img-container p-5"
-         onClick={()=> console.log("you clicked on the image container")}>
-        <Link to="/details">
-        <img src={img} alt="product" className="card-img-top" />
 
-        </Link>
-        <button
-          className="cart-btn"
-          disabled={inCart ? true : false}
-          onClick={() =>{ console.log("added to the cart");
-          }}>
-        {inCart? (
-          <p className="text-capitalize mb-0" disabled>
-          {" "}
-          in cart
-        </p>
-      ):(
-        <i className="fas fa-cart-plus" />
-      )}
-        </button>
-      </div>
+      <ProductConsumer>
+        {(value)=> (<div className="img-container p-5"
+           onClick={()=>
+             value.handleDetail(id)
+           }>
+          <Link to="/details">
+          <img src={img} alt="product" className="card-img-top" />
+
+          </Link>
+          <button
+            className="cart-btn"
+            disabled={inCart ? true : false}
+            onClick={() =>{
+              value.addToCart(id);
+            }}>
+          {inCart? (
+            <p className="text-capitalize mb-0" disabled>
+            {" "}
+            in cart
+          </p>
+        ):(
+          <i className="fas fa-cart-plus" />
+        )}
+          </button>
+        </div>
+)}
+
+      </ProductConsumer>
       {/*card footer */}
       <div className="card-footer d-flex
         justifty-content-between">
@@ -37,8 +45,8 @@ export default class Product extends Component{
         {title}
         </p>
         <h5 className="text-blue font-italic mb-0">
-        <span className="mr-1">$</span>
-          {price}
+        <span className="mr-1"> $ </span>
+        {price}
         </h5>
       </div>
       </div>
@@ -70,7 +78,30 @@ const ProductWrapper = styled.div`
   position:relative;
   overflow:hidden;
 }
+.card-img-top{
+    transition:all 1s linear;
+}
 .img-container:hover .card-img-top{
   transform:scale(1.2);
+}
+.cart-btn{
+  position:absolute;
+  bottom:0;
+  right:0;
+  padding:0.2rem 0.4rem;
+  background: var(--lightBlue);
+  border:none;
+  color:var(--mainWhite);
+  font-size: 1.4rem;
+  border-radius: 0.5rem 0 0 0;
+  transform:translate(100%,100%);
+  transition:all 1s linear;
+}
+.img-container:hover .cart-btn{
+  transform:translate(0,0);
+}
+.cart-btn:hover{
+  color:var(--mainBlue);
+  cursor: pointer;
 }
 `;
