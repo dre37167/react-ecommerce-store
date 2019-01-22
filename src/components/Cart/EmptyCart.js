b949ae3480270e7fb3ea0da903b11b571eb34f2a
dre37167 <tidre37167@gmail.com>
2019-01-21 19:06:41 -0700

    removed item button, increment and decrement buttons are set

---
+++ src/components/Cart/EmptyCart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function EmptyCart(){
+  return (
+    <div classname="container mt-5">
+      <div classname="row">
+      <div classname="col-10 mx-auto text-center text-title">
+      <h1>your cart is currently empty</h1>
+      </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Default.js
+++ src/components/Default.js
@@ -4,6 +4,6 @@ export default class extends Component{
   render(){
     return(
       <div><h3>Page not found</h3></div>
-    )
+    );
   }
 }
